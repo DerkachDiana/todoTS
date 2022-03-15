99b230e80169ce4050a7d2dd817ef6e74d4b2bb2
Alexander Butov <alexander.butov@devexperts.com>
2022-03-15 17:13:19 +0300

    fix ts2705 bug and add eslint

--- frontend/todo.ts
+++ frontend/todo.ts
@@ -1,7 +1,7 @@
 import TodoAPI from './TodoAPI'
 
-const addButton : HTMLElement | null = document.getElementById("add-button") ;
-const addInputArea = document.getElementById("add-input");
+const addButton : HTMLButtonElement | null = document.getElementById("add-button") ;
+const addInputArea : HTMLInputElement | null = document.getElementById("add-input");
 const addArea : HTMLElement | null = document.querySelector(".background__white-area__add-area");
 
  let taskCounter : number;
@@ -10,13 +10,13 @@ const addArea : HTMLElement | null = document.querySelector(".background__white-
  addArea.addEventListener("submit", clearTextByPressEnter);
 
  interface Task {
-   _id: Number,
-   text: String,
-   checked: Boolean
+   _id: string,
+   text: string,
+   checked: boolean
  }
 
 async function addTaskToList()  {
-  if (addInputArea.nodeValue === '') {
+  if (addInputArea.value === '') {
     return;
   }
 
@@ -27,9 +27,9 @@ async function addTaskToList()  {
 
   try {
     const createdTask = await TodoAPI.createTask(task);
-    console.log(createdTask)
 
     renderTodo(createdTask);
+
     taskCounter++;
   } catch (error) {
     console.log("addTaskToList error " + error);
@@ -42,98 +42,98 @@ function renderTodo(task : Task) : void {
   document.getElementById(String(task._id)).addEventListener("click", () => taskDeleter(task._id));
 
   const checkboxContext = document.getElementById(task._id).previousElementSibling.childNodes[1];
-  const inputboxContext = checkboxContext.nextElementSibling;
+  const inputboxContext : HTMLInputElement | null = checkboxContext.nextElementSibling;
 
   inputboxContext.addEventListener("blur", () => updateTaskOnBlur(inputboxContext));
 
   renderCheckbox(checkboxContext, task.checked);
 }
-// function renderCheckbox(context, isChecked) {
-//   context.addEventListener("click", async () => {
-//     try {
-//       await updateTaskOnCheck(context)
-//     } catch (error) {
-//       console.log("renderCheckbox error " + error)
-//     }
-//   });
-//   context.checked = isChecked;
-//   changeTextStyle(context);
-// }
-// async function taskDeleter(taskId) {
-//   document.getElementById(taskId).parentElement.remove();
-//   try {
-//     await TodoAPI.deleteTask(taskId);
-//   } catch(error) {
-//     console.log(error)
-//   }
-//   taskCounter--;
-// }
-// function createHtmlBlockOfTask(task) {
-//   return `<div class="background__white-area__task-list__task">\n` +
-//     '                            <div class="background__white-area__task-list__task__left-part">\n' +
-//     `                               <input type="checkbox" id="checkbox" class="background__white-area__task-list__task__checkbox checkbox">\n` +
-//     `                                <div class="background__white-area__task-list__task__text" contenteditable="true">${task.text}</div>\n` +
-//     '                            </div>\n' +
-//     `                            <button id="${task._id}" class="background__white-area__task-list__task__delete-button button-style">Delete</button>\n` +
-//     '                        </div>'
-// }
-// async function renderCounter() {
-//   try {
-//     const tasks = await TodoAPI.getAllTasks();
-//     document.getElementById("number-of-tasks").innerText = tasks.length;
-//   } catch(error) {
-//     console.log("getCount error " + error)
-//   }
-// }
-// function clearTextByPressEnter() {
-//   event.preventDefault();
-//   addButton.addEventListener('click', addTaskToList);
-//   addInputArea.value = '';
-// }
-// async function updateTaskOnBlur(context) {
-//   const taskId = context.parentNode.parentNode.lastElementChild.id;
-//   const taskText = context.textContent;
-//   const taskChecked = context.previousElementSibling.checked;
-//
-//   if (taskText.trim() === '') {
-//     return;
-//   }
-//
-//   try {
-//     await TodoAPI.updateTasks(taskId, taskText, taskChecked);
-//     changeTextStyle(context);
-//   } catch (error) {
-//     console.log("updateTaskOnCheck error " + error)
-//   }
-// }
-// async function updateTaskOnCheck(context) {
-//   try {
-//     const taskId = context.parentNode.parentNode.lastElementChild.id;
-//     const taskText = context.nextElementSibling.textContent
-//     await TodoAPI.updateTasks(taskId, taskText, context.checked);
-//     changeTextStyle(context);
-//   } catch (error) {
-//     console.log("updateTaskOnCheck error " + error)
-//   }
-//
-// }
-// function changeTextStyle(element) {
-//   if (element.checked) {
-//     element.parentNode.childNodes[3].style.textDecoration = "line-through";
-//     element.parentNode.childNodes[3].style.color = "gray";
-//   } else {
-//     element.parentNode.lastElementChild.style.textDecoration = "none";
-//     element.parentNode.childNodes[3].style.color = "black";
-//   }
-// }
-// async function loadTaskList() {
-//   try {
-//     const tasks = await TodoAPI.getAllTasks();
-//     tasks.forEach(task => renderTodo(task));
-//     taskCounter = await renderCounter();
-//     console.log(tasks)
-//   } catch (error) {
-//     console.log("loadTaskList error" + error)
-//   }
-// }
-// document.addEventListener("DOMContentLoaded", loadTaskList)
+function renderCheckbox(context, isChecked) {
+  context.addEventListener("click", async () => {
+    try {
+      await updateTaskOnCheck(context)
+    } catch (error) {
+      console.log("renderCheckbox error " + error)
+    }
+  });
+  context.checked = isChecked;
+  changeTextStyle(context);
+}
+async function taskDeleter(taskId) {
+  document.getElementById(taskId).parentElement.remove();
+  try {
+    await TodoAPI.deleteTask(taskId);
+  } catch(error) {
+    console.log(error)
+  }
+  taskCounter--;
+}
+function createHtmlBlockOfTask(task : Task) {
+  return `<div class="background__white-area__task-list__task">\n` +
+    '                            <div class="background__white-area__task-list__task__left-part">\n' +
+    `                               <input type="checkbox" id="checkbox" class="background__white-area__task-list__task__checkbox checkbox">\n` +
+    `                                <div class="background__white-area__task-list__task__text" contenteditable="true">${task.text}</div>\n` +
+    '                            </div>\n' +
+    `                            <button id="${task._id}" class="background__white-area__task-list__task__delete-button button-style">Delete</button>\n` +
+    '                        </div>'
+}
+async function renderCounter() {
+  try {
+    const tasks = await TodoAPI.getAllTasks();
+    document.getElementById("number-of-tasks").innerText = tasks.length;
+  } catch(error) {
+    console.log("getCount error " + error)
+  }
+}
+function clearTextByPressEnter() {
+  event.preventDefault();
+  addButton.addEventListener('click', addTaskToList);
+  addInputArea.value = '';
+}
+async function updateTaskOnBlur(context) {
+  const taskId = context.parentNode.parentNode.lastElementChild.id;
+  const taskText = context.textContent;
+  const taskChecked = context.previousElementSibling.checked;
+
+  if (taskText.trim() === '') {
+    return;
+  }
+
+  try {
+    await TodoAPI.updateTasks(taskId, taskText, taskChecked);
+    changeTextStyle(context);
+  } catch (error) {
+    console.log("updateTaskOnCheck error " + error)
+  }
+}
+async function updateTaskOnCheck(context : HTMLInputElement) {
+  try {
+    const taskId = context.parentNode.parentNode.lastElementChild.id;
+    const taskText = context.nextElementSibling.textContent
+    await TodoAPI.updateTasks(taskId, taskText, context.checked);
+    changeTextStyle(context);
+  } catch (error) {
+    console.log("updateTaskOnCheck error " + error)
+  }
+
+}
+function changeTextStyle(element : HTMLElement) {
+  if (element.checked) {
+    element.parentNode.childNodes[3].style.textDecoration = "line-through";
+    element.parentNode.childNodes[3].style.color = "gray";
+  } else {
+    element.parentNode.lastElementChild.style.textDecoration = "none";
+    element.parentNode.childNodes[3].style.color = "black";
+  }
+}
+async function loadTaskList() {
+  try {
+    const tasks = await TodoAPI.getAllTasks();
+    tasks.forEach(task => renderTodo(task));
+    taskCounter = await renderCounter();
+    console.log(tasks)
+  } catch (error) {
+    console.log("loadTaskList error" + error)
+  }
+}
+document.addEventListener("DOMContentLoaded", loadTaskList)
